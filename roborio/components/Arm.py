1f36ede878d32c783a448cd6bff0288bab5910a1
GreysonLove0 <greysonlove2010@gmail.com>
2025-10-06 19:21:18 -0500

    fixed errors

--- roborio/components/Arm.py
+++ roborio/components/Arm.py
@@ -1,11 +1,13 @@
-from phoenix6.hardware import talonFX
-class MyComponent:
-	def __init__(self):
-		#initialized the component.
+from phoenix6.hardware import TalonFX
 
-		self.name = "motor"
-		self.motor = talonFX(0)
-        #info methods about motor
-		
-	def set_position(self, position):
-    self.motor.set_position(position)
+
+class MyComponent:
+    def __init__(self):
+        # initialized the component.
+
+        self.name = "motor"
+        self.motor = TalonFX(0)
+        # info methods about motor
+
+    def set_position(self, position):
+        self.motor.set_position()
