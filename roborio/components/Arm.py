bdcaace490b4dee8439631badcd46fd8d0679e2a
GreysonLove0 <greysonlove2010@gmail.com>
2025-10-23 15:02:50 -0500

    Addad a range of motion to the motor and configured the motor, cannot get the motor to configure properly and imports arent going through.

--- roborio/components/Arm.py
+++ roborio/components/Arm.py
@@ -1,13 +1,49 @@
 from phoenix6.hardware import TalonFX
-
+from FROGlib.ctre import FROGTalonFX, FROGTalonFXConfig, FROGFeedbackConfig
+from phoenix6.configs import (
+    Slot0Configs,
+    Slot1Configs,
+    MotorOutputConfigs,
+    SoftwareLimitSwitchConfigs,
+    MotionMagicConfigs,
+)
 
 class MyComponent:
     def __init__(self):
-        # initialized the component.
-
+    # initialize the component.
         self.name = "motor"
-        self.motor = TalonFX(0)
-        # info methods about motor
+        self.motor = FROGTalonFX(
+            id: int=1,
+            motor_config: FROGTalonFXConfig = FROGTalonFXConfig(
+            feedback_config=FROGFeedbackConfig(),
+            slot1gains=Slot1Configs()
+                .with_k_a(1)
+                .with_k_p(1)
+                .with_k_s(1)
+                .with_k_v(1)
+                .with_k_i(1), # Need to edit the values once i found out what they should be.
+            )
+            parent_nt="Arm",
+            motor_name="motor",
+    # Configure the motor
+        )
 
-    def set_position(self, position):
-        self.motor.set_position()
+
+
+    def reset_position(self):
+        self.motor.set_position(0)
+        self._enable_software_limits()
+
+    def _enable_software_limits(self):
+        self.motor.config.with_software_limit_switch(
+            SoftwareLimitSwitchConfigs()
+            .with_reverse_soft_limit_enable(True)
+            .with_reverse_soft_limit_threshold(0.0)
+            .with_forward_soft_limit_enable(True)
+            .with_forward_soft_limit_threshold(1)
+        )
+    #Set motor range of motion.
+
+    # Set home/starting Position.
+
+    # Have two set positions for motor to go to.
